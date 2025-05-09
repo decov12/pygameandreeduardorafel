5514eba8ab78f2fca611b268a45335d9253daa23
Eduardo <eduardoca5@al.insper.edu.br>
2025-05-09 14:28:05 -0300

    loop principal do jogo

--- main.py
+++ main.py
@@ -6,3 +6,22 @@ pygame.init
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Crossy Road")
 clock = pygame.time.Clock()
+
+# Loop principal do pygame:
+
+running = True
+while running:
+    clock.tick(FPS)
+
+    # eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Atualizacoes
+
+    # Desenhos
+    window.fill(WHITE)
+
+    pygame.display.update()
+pygame.quit()

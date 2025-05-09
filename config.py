c8b3cdc9d811ef75f0c70c5a5fc6a138cc1c8362
Eduardo <eduardoca5@al.insper.edu.br>
2025-05-09 14:07:07 -0300

    tamanho,cores,fps,velocidade jogador

---
+++ config.py
@@ -0,0 +1,16 @@
+#  TAMANHO DA JANELA 
+WIDTH = 800
+HEIGHT = 600
+
+# cores
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+# FPS
+FPS = 60 
+
+# Velocidade do jogador
+PLAYER_SPEED = 5

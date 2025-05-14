1563a3383c2caa482860e00f4b7cb948c9befa69
André Vasconcellos <andretbv@al.insper.edu.br>
2025-05-14 08:01:47 -0300

    adição do tronco de madeira

--- assets.py
+++ assets.py
@@ -1,6 +1,6 @@
 import pygame
 import random
-from config import WIDTH, HEIGHT, WHITE, FPS, PLAYER_WIDTH, PLAYER_HEIGHT, CAR_WIDTH, CAR_HEIGHT 
+from config import WIDTH, HEIGHT, WHITE, FPS, PLAYER_WIDTH, PLAYER_HEIGHT, CAR_WIDTH, CAR_HEIGHT, WOOD_WIDTH, WOOD_HEIGHT
 
 assets = {}
 assets['player_image'] = pygame.image.load('assets/Armature_jumping_left_1.png').convert_alpha()
@@ -14,5 +14,7 @@ assets['car_brown'] = pygame.image.load('assets/brown car front.png').convert_al
 assets['car_brown'] = pygame.transform.scale(assets['car_brown'], (CAR_WIDTH, CAR_HEIGHT))
 assets['car_red'] = pygame.image.load('assets/red car front.png').convert_alpha()
 assets['car_red'] = pygame.transform.scale(assets['car_red'], (CAR_WIDTH, CAR_HEIGHT))
+assets['tree_log']=pygame.image.load('assets/the wood.png').convert_alpha()
+assets['tree_log']=pygame.transform.scale(assets['tree_log'], (WOOD_WIDTH,WOOD_HEIGHT))
 
 assets['background'] = pygame.image.load('assets/Game Level.png').convert_alpha()
